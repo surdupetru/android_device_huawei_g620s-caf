8c5afc829ab1ae3428fb526dbe2eb2032aa7b473
surdupetru <surdu_petru@yahoo.com>
2016-02-07 20:19:34 +0100

    update Architecture Flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,11 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_BOARD_SUFFIX := _32
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Audio
 USE_CUSTOM_AUDIO_POLICY := 1
